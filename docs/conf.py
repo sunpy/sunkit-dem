b4ee791685cbbbf3b10634f7d7518c576a92229f
Nick Murphy <namurphy@cfa.harvard.edu>
2020-04-23 17:04:44 -0400

    Add ndcube to intersphinx mapper
    Co-Authored-By: Will Barnes <will.t.barnes@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,8 @@ intersphinx_mapping = {
     'matplotlib': ('https://matplotlib.org/',
                    (None, 'http://data.astropy.org/intersphinx/matplotlib.inv')),
     'astropy': ('http://docs.astropy.org/en/stable/', None),
-    'sunpy': ('https://docs.sunpy.org/en/stable/', None)}
+    'sunpy': ('https://docs.sunpy.org/en/stable/', None),
+    'ndcube':('https://docs.sunpy.org/projects/ndcube/en/stable/', None)}
 
 # -- Options for HTML output -------------------------------------------------
 
