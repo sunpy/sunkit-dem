bce55eb8fe0219666d59027c390559417b4110ca
Will Barnes <will.t.barnes@gmail.com>
2021-03-11 21:26:00 -0500

    add some tests

---
+++ sunkit_dem/tests/test_util.py
@@ -0,0 +1,30 @@
+"""
+Tests for utilities
+"""
+import pytest
+import numpy as np
+import astropy.units as u
+
+from sunkit_dem.util import quantity_1d_to_sequence
+
+wavelengths = np.linspace(0, 1, 10) * u.angstrom
+intensities = np.random.rand(wavelengths.shape[0]) * u.ct
+
+
+@pytest.fixture
+def sequence1d():
+    return quantity_1d_to_sequence(intensities, wavelengths)
+
+
+def test_dimensions(sequence1d):
+    assert all(sequence1d.cube_like_dimensions == [10]*u.pix)
+
+
+def test_common_axis(sequence1d):
+    common_axis = u.Quantity(sequence1d.common_axis_coords[0])
+    assert common_axis.shape == wavelengths.shape
+    assert u.allclose(common_axis, wavelengths, rtol=1e-10)
+
+
+def test_axis_type(sequence1d):
+    assert sequence1d.cube_like_array_axis_physical_types == [('em.wl',)]
