c07799a13a1bd8f4ded2c2c96f04facd1034ca9e
Will Barnes <will.t.barnes@gmail.com>
2021-03-11 21:25:48 -0500

    world axis type fixes

--- sunkit_dem/base_model.py
+++ sunkit_dem/base_model.py
@@ -30,9 +30,10 @@ class GenericModel(BaseModel):
     Parameters
     ----------
     data : `~ndcube.NDCubeSequence`
-    kernel : `dict`
-        Each entry should be an `~astropy.units.Quantity` with a key that
-        denotes the wavelength
+    kernel : `list`
+        List of `~astropy.units.Quantity` objects containing the kernels
+        of each response. The order must be the same as the wavelength
+        sequence ordering of `data`.
     temperature_bin_edges : `~astropy.units.Quantity`
         Edges of the temperature bins in which the DEM is computed. The
         rightmost edge is included. The kernel is evaluated at the bin centers.
@@ -98,7 +99,7 @@ class GenericModel(BaseModel):
             raise u.UnitsError('Each NDCube in NDCubeSequence must have units')
         # TODO: check that all WCS are the same, within some tolerance
         # NOTE: not always wavelength? Could be energy, filter wheel, etc.
-        if data.cube_like_world_axis_physical_types[0] != 'em.wl':
+        if data.cube_like_array_axis_physical_types[0] != ('em.wl',):
             raise ValueError('First axis of sequence must be wavelength.')
         self._data = data
 
@@ -110,15 +111,14 @@ class GenericModel(BaseModel):
     def kernel(self, kernel):
         if len(kernel) != self.data.cube_like_dimensions[0].value:
             raise ValueError('Number of kernels must be equal to length of wavelength dimension.')
-        if not all([kernel[k].shape == self.temperature_bin_centers.shape for k in kernel]):
+        if not all([k.shape == self.temperature_bin_centers.shape for k in kernel]):
             raise ValueError('Temperature and kernels must have the same shape.')
         self._kernel = kernel
 
     @property
     def wavelength(self):
         # TODO: handle non-numeric "wavelength" designations
-        return u.Quantity(self.data.common_axis_extra_coords['wavelength'],
-                          self.data[0].wcs.wcs.cunit[-1])
+        return u.Quantity(self.data.common_axis_coords[0], self.data[0].wcs.wcs.cunit[-1])
 
     @property
     def data_matrix(self):
@@ -126,8 +126,7 @@ class GenericModel(BaseModel):
 
     @property
     def kernel_matrix(self):
-        return u.Quantity(np.stack([self.kernel[w].value for w in self.wavelength.value]),
-                          self.kernel[self.wavelength.value[0]].unit)
+        return u.Quantity(np.stack([k.value for k in self.kernel]), self.kernel[0].unit)
 
     def fit(self, *args, **kwargs):
         """
@@ -145,8 +144,7 @@ class GenericModel(BaseModel):
         meta = self._make_dem_meta()
         # NOTE: Bug in NDData that does not allow passing quantity as uncertainty
         uncertainty = uncertainty.value if isinstance(uncertainty, u.Quantity) else uncertainty
-        return ndcube.NDCube(dem, wcs, meta=meta, uncertainty=uncertainty,
-                             missing_axes=self.data[0].missing_axes)
+        return ndcube.NDCube(dem, wcs, meta=meta, uncertainty=uncertainty,)
 
     def _make_dem_wcs(self):
         # NOTE: Assumes that WCS for all cubes is the same
